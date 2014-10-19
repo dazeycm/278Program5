38b1dae0b5eb00a72e8b18599d9b6e227b2d9d46
Craig Dazey <dazeycm@miamioh.edu>
2014-10-19 17:02:13 -0400

    Finished, but with questions still

--- program5.asm
+++ program5.asm
@@ -9,25 +9,109 @@
 
 #can then call the function and freely use the temp registers
 
+#store number bigget than 32 bit allows
+#need to restore ra and fp in main method?
+#should the local variables be stored in S? (currently stored in temp memory) If this is yes, then I should put the result of sum in an S registed too
+
+	.data
+out:	.asciiz	"The factorial (sum) of "
+out2:	.asciiz " is "
+
+
 	.text
+	.globl main
 main: 
 	subi $sp, $sp, 60
 	sw $ra, 4($sp)
 	sw $fp, 8($sp)
 	addi $fp, $sp, 56
 	
-	li $s0, 5
-	li $s1, 10
-	li $s2, 15
-	li $s3, 20
-	li $s4, 25
+	li $a0, 5
+	li $a1, 10
+	li $a2, 15
+	li $a3, 20
+	li $s1, 25
 	
-	sw $s0, 12($sp)
-	sw $s1, 16($sp)
-	sw $s2, 20($sp)
-	sw $s3, 24($sp)
-	sw $s4, 28($sp)
+	jal sum
+	
+	sw $v0, 12($sp)	#store return value of sum
+	
+	la $a0, out
+	li $v0, 4
+	syscall
+	
+	move $a0, $s6	
+	li $v0, 1	
+	syscall		#THE FANCY THING WORKS!!!!
+	
+	la $a0, out2
+	li $v0, 4
+	syscall
+	
+	lw $v0, 12($sp)	#load value of sum
+	
+	move $a0, $v0
+	li $v0, 1
+	syscall
 	
 	
+	lw $ra, 4($sp)
+	lw $fp, 8($sp)
+	addi $fp, $sp, 56
 	
+	j exit		#time to leave town
 	
+	.text
+sum:
+	subu $sp, $sp, 32
+	sw $ra, 20($sp)
+	sw $fp, 16($sp)
+	addi $fp, $sp, 28
+	
+	add $t1, $a0, $a1
+	add $t1, $t1, $a2
+	add $t1, $t1, $a3
+	add $t1, $t1, $s1
+	
+	div $t1, $t1, 5
+	move $a0, $t1
+	jal factorial
+	
+	lw $ra, 20($sp)
+	lw $fp, 16($sp)
+	addi $sp, $sp, 32
+	
+	jr $ra
+
+factorial: 			#using frame pointer to move around stack frame (not that it matters)
+	subu $sp, $sp, 32
+	sw $ra, 20($sp)
+	sw $fp, 16($sp)
+	addi $fp, $sp, 28
+	sw $a0, 0($fp)
+	
+	lw $v0, 0($fp)
+	bgtz $v0, L2
+	li $v0, 1
+	
+	j L1
+	
+L2:
+	lw $v1, 0($fp)
+	subi $v0, $v1, 1
+	move $a0, $v0
+	jal factorial
+	
+	lw $v1, 0($fp)
+	mul $v0, $v0, $v1
+	
+L1:
+	move $s6, $v1	#trying to be fancy
+
+	lw $ra, 20($sp)
+	lw $fp, 16($sp)
+	addi $sp, $sp, 32
+		
+	jr $ra
+	
+exit:
